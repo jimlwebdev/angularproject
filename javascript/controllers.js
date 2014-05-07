7e37b4262ade8d1e34c481e398031769a2bedea9
jiml <jiml.webdev@gmail.com>
2014-05-06 20:52:20 -0400

    add views to project

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -1,4 +1,19 @@
+'use strict';
+
 		jiml.namespace("jiml.controller");
 		jiml.controller.function1 = function($scope){
 		//	 $scope.greeting = { text: 'Hello' };
 		}
+		// declare top-level module which depends on filters,and services
+		var sampleApp = angular.module('sampleApp',['ngRoute']);
+		sampleApp.config(['$routeProvider',
+		  function($routeProvider){
+			$routeProvider.
+			when('/', {
+				templateUrl:'views/index.html'
+			}).
+			otherwise({
+				redirectTo:'views/index.html'
+			});
+		  }
+		]);
