a5e106622ddadcb1548621b1dfca4f36975de8d7
jiml <jiml.webdev@gmail.com>
2014-05-07 21:00:28 -0400

    add search

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -1,13 +1,8 @@
-'use strict';
-
-		jiml.namespace("jiml.controller");
-		jiml.controller.function1 = function($scope){
-		//	 $scope.greeting = { text: 'Hello' };
-		}
-		// declare top-level module which depends on filters,and services
-		var sampleApp = angular.module('sampleApp',['ngRoute']);
-		sampleApp.config(['$routeProvider',
-		  function($routeProvider){
+		'use strict';
+		jiml.namespace("jiml.module");
+		jiml.module.sampleApp = angular.module('sampleApp',['ngRoute']);
+		
+		jiml.module.routeProvider = function($routeProvider){
 			$routeProvider.
 			when('/', {
 				templateUrl:'views/index.html'
@@ -15,5 +10,60 @@
 			otherwise({
 				redirectTo:'views/index.html'
 			});
-		  }
-		]);
+		  };
+		
+		jiml.module.sampleApp.config(['$routeProvider',jiml.module.routeProvider]);
+
+		jiml.module.function1 = function($scope){
+		//	 $scope.greeting = { text: 'Hello' };
+		}	
+
+		jiml.module.sampleApp.controller(
+			"sampleController",
+			function($scope, searchService){
+				$scope.searchResults = {};
+				searchService.getSearch()
+					.then(
+						function(results){
+							$scope.searchResults = results;
+						}
+					)
+			}
+		)
+		jiml.module.sampleApp.service("searchService",
+			function( $http, $q ) {
+
+				return({
+					getSearch: getSearch
+				});
+				// I get all of the friends in the remote collection.
+				function getSearch() {
+
+					var request = $http({
+						method: "get",
+						url: "http://tmb_rpc.qa.pdone.com/search_processor.php?mode=SEARCH&page=1&phrase=e&page_length=100&type=news",
+					});
+
+					return( request.then( handleSuccess, handleError ) );
+
+				}
+				
+				function handleError( response ) {
+					if (
+						! angular.isObject( response.data ) ||
+						! response.data.message
+						) {
+
+						return( $q.reject( "An unknown error occurred." ) );
+
+					}
+					return( $q.reject( response.data.message ) );
+
+				}
+				function handleSuccess( response ) {
+
+					return( response.data );
+
+				}
+			}
+		);
